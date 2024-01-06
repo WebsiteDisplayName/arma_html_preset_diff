3ee871271559dcac11118bc6742997a5194277f5
WebsiteDisplayName <rowemg@bc.edu>
2024-01-05 22:38:18 -0500

    combining files

--- html_diff_arma.py
+++ html_diff_arma.py
@@ -5,16 +5,36 @@ from bs4 import BeautifulSoup
 # Opening the html file
 # existing_op_falling_water.html
 # new_op_falling_water.html
-input_html = r"new_op_falling_water"
-HTMLFile = open(input_html+".html", "r") 
+
+file_1_name = r"new_op_falling_water"
+file_2_name = r"existing_op_falling_water"
+
+def ret_mod_names(html_name: str) -> None:
+  HTMLFile = open(html_name+".html", "r") 
+    
+  # Reading the file 
+  index = HTMLFile.read() 
+    
+  # Creating a BeautifulSoup object and specifying the parser 
+  S = BeautifulSoup(index, 'lxml') 
+    
+  # Using the find_all method to find all elements of a tag 
+  with open(html_name + '.txt', 'w') as f:
+    for tag in S.find_all('td', {'data-type': 'DisplayName'}): 
+        f.write(f'{tag.text}\n')
   
-# Reading the file 
-index = HTMLFile.read() 
-  
-# Creating a BeautifulSoup object and specifying the parser 
-S = BeautifulSoup(index, 'lxml') 
-  
-# Using the find_all method to find all elements of a tag 
-with open(input_html + '.txt', 'w') as f:
-  for tag in S.find_all('td', {'data-type': 'DisplayName'}): 
-      f.write(f'{tag.text}\n')
+  return None
+
+ret_mod_names(file_1_name)
+ret_mod_names(file_2_name)
+
+file_1_txt = open(file_1_name+".txt","r")
+file_2_txt = open(file_2_name+".txt","r")
+list_1 = file_1_txt.readlines()
+list_2 = file_2_txt.readlines()
+
+print("file_1_unique")
+list_1_unique = [print(mod_name) for mod_name in list_1 if mod_name not in list_2]
+
+print("\nfile_2_unique")
+list_2_unique = [print(mod_name) for mod_name in list_2 if mod_name not in list_1]
