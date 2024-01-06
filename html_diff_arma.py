7389d1b915ec582091ed3e1e3ea9ca9be5c76884
WebsiteDisplayName <rowemg@bc.edu>
2024-01-06 00:07:34 -0500

    print output removed \n

--- html_diff_arma.py
+++ html_diff_arma.py
@@ -6,8 +6,8 @@ from bs4 import BeautifulSoup
 # existing_op_falling_water.html
 # new_op_falling_water.html
 
-file_1_name = r"new_op_falling_water"
-file_2_name = r"existing_op_falling_water"
+file_1_name = r"Op_Falling_Water_manually_added"
+file_2_name = r"Op_Falling_Water"
 
 def ret_mod_names(html_name: str) -> None:
   HTMLFile = open(html_name+".html", "r") 
@@ -34,7 +34,7 @@ list_1 = file_1_txt.readlines()
 list_2 = file_2_txt.readlines()
 
 print("file_1_unique")
-list_1_unique = [print(mod_name) for mod_name in list_1 if mod_name not in list_2]
+list_1_unique = [print(mod_name, end='') for mod_name in list_1 if mod_name not in list_2]
 
 print("\nfile_2_unique")
-list_2_unique = [print(mod_name) for mod_name in list_2 if mod_name not in list_1]
+list_2_unique = [print(mod_name, end='') for mod_name in list_2 if mod_name not in list_1]
